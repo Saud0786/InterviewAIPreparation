d74147a7cf4d75b2f83976fcdc090b464d6aa759
Saud0786 <saud31253@gmail.com>
2025-05-30 16:49:36 +0530

    seond hosting

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './Pages/LandingPage';
 // import Login from './pages/Auth/Login';
 // import SignUp from './pages/Auth/SignUp';
 import InterviewPrep from './Pages/InterviewPrep/InterviewPrep';
